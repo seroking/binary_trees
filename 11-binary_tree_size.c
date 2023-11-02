487a44ed84eca5ac4e75dfdffb9b347254b6482b
seroking <baridmohamedamine004@gmail.com>
2023-11-02 16:16:22 +0100

    binary trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,5 +14,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t left = binary_tree_size(tree->left);
 	size_t right = binary_tree_size(tree->right);
 
-	return (1 + left + right);
+	return (left + right + 1);
 }
