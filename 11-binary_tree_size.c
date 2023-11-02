b96d44c6cd937c3400d3ad4b5e1f34ebc5fdf337
seroking <baridmohamedamine004@gmail.com>
2023-11-02 16:49:36 +0100

    binary trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /*
- * binary_tree_size - measure the size of the binary tree
+ *binary_tree_size - measure the size of the binary tree
  *
  * @tree: pointer to tree
- * Return: size
+ * Return: size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
